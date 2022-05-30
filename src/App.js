ccb5a57f544234e69114cc637e007c04c5979142
Burcu Saglam <saglamburcu96@gmail.com>
2022-05-30 19:37:01 +0300

    The project was completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [sliceParagraphs, setSliceParagraphs] = useState([]);
+
+  const onChangeParagraph = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(number);
+
+    if (number <= 0) {
+      amount = 1;
+    };
+
+    if (number > 8) {
+      amount = 8;
+    }
+
+    let newParagraph = data.slice(0, amount);
+    setSliceParagraphs(newParagraph);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <section className="title">
+        TIRED OF BORING LOREM IPSUM?
+      </section>
+      <section className="form">
+        <form onSubmit={(e) => onChangeParagraph(e)}>
+          <label>Paragraphs:</label>
+          <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+          <button type="submit">GENERATE</button>
+        </form>
+      </section>
+      <section className="paragraphs">
+        {
+          sliceParagraphs.map((sliceParagraph, index) => {
+            return <p key={index} >{sliceParagraph}</p>
+          })
+        }
+      </section>
+    </main >
   );
-}
+};
 
 export default App;
